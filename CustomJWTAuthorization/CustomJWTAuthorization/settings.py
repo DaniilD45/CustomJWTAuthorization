b30b2e091a02eae1be8fe22f947c3b463fbf218c
Daniil45 <findmad41@gmail.com>
2026-03-31 04:13:32 +0700

    Containerize Django app with PostgreSQL integration, ref #1

--- CustomJWTAuthorization/CustomJWTAuthorization/settings.py
+++ CustomJWTAuthorization/CustomJWTAuthorization/settings.py
@@ -27,9 +27,9 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', ]
 
 
 # Application definition
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'CustomJWTAuthorization.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('POSTGRES_HOST'),
+        'PORT': env('POSTGRES_PORT'),
     }
 }
 
---
+++ CustomJWTAuthorization/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+echo "Applying database migrations..."
+python manage.py migrate --noinput
+
+echo "Creating superuser if not exists..."
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); \
+User.objects.filter(username='admin').exists() or \
+    User.objects.create_superuser(username='admin', password='adminpass1337')" | python manage.py shell
+
+exec "$@"
